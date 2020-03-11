3d5621c7adaaf9810ed7260b152a9437c8dbb230
Vandervert <242219@xcelenergy.com>
2020-03-11 14:24:19 -0600

    Add project files.

---
+++ AFSDKDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AFSDKDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
